1025f48fc22b9768c6d806ec047c05f1a060bd81
Yaqoob <yaqoob@gmail.com>
2022-12-06 11:11:24 +0500

    FB Ads Config

--- app/src/main/java/com/orbitalsonic/facebookadsconfiguration/MainApplication.kt
+++ app/src/main/java/com/orbitalsonic/facebookadsconfiguration/MainApplication.kt
@@ -11,9 +11,9 @@ class MainApplication:Application() {
         AudienceNetworkAds.initialize(this)
         /**
          * after run application
-         * search addTestDevice in your logcat
+         * search addTestDevice in your android studio logcat
          * and add test device here
-         * after uninstall the app every time id has different
+         * after uninstall the app test device id changed
          */
         AdSettings.addTestDevice("3596a42d-0d02-4624-83a8-daaeafdec280")
     }
